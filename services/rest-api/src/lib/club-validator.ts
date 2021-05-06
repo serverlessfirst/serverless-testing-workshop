ba559ecad4a40fd804ff3295aac060e2e3181d27
Paul Swail <paul@winterwindsoftware.com>
2021-05-06 21:42:38 +0100

    Exercise resets (#21)
    * week2 solution code
    
    * week3 solution code
    
    * week4 solution code
    
    * skip very slow running DLQ test

--- services/rest-api/src/lib/club-validator.ts
+++ services/rest-api/src/lib/club-validator.ts
@@ -1,9 +1,3 @@
 import { Club } from './types/sports-club-manager';
 
-const isAlphanumericOrSpace = (s: string) => /^[a-z0-9\s]+$/i.test(s);
-
-export const validateClub = (club: Club) =>
-  !!club.name
-  && club.name.length > 2
-  && club.name.length <= 64
-  && isAlphanumericOrSpace(club.name);
+export const validateClub = (club: Club) => !!club.name && club.name.length > 2 && club.name.length <= 64;
--- services/rest-api/tests/unit/club-validator.spec.ts
+++ services/rest-api/tests/unit/club-validator.spec.ts
@@ -27,27 +27,5 @@ describe('club-validator', () => {
     expect(result).toBe(false);
   });
 
-  it('rejects club names that contain non-alphanumeric characters (e.g. emojis)', () => {
-    const club = {
-      id: uuidv4(),
-      name: 'F⚽️⚽️tball',
-      sport: 'Soccer',
-      visibility: ClubVisibility.PRIVATE,
-      managerId: '123456789',
-    };
-    const result = validateClub(club);
-    expect(result).toBe(false);
-  });
-
-  it('allows names of correct length that contain alphanumeric characters and space', () => {
-    const club = {
-      id: uuidv4(),
-      name: 'Manchester United',
-      sport: 'Soccer',
-      visibility: ClubVisibility.PRIVATE,
-      managerId: '123456789',
-    };
-    const result = validateClub(club);
-    expect(result).toBe(true);
-  });
+  it.todo('rejects club names that contain non-alphanumeric characters (e.g. emojis)');
 });
